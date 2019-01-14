2dde84214eb2debaba356467b2ecaba2e3643f86
zhaohuapeng <zhaohuapeng@panda.tv>
2019-01-14 18:08:23 +0800

    done

--- extension.js
+++ extension.js
@@ -8,8 +8,17 @@ const uploader = require('./lib/upload')
 const utils = require('./lib/util')
 const fs = require('fs')
 
-function saveLocal(source, dist) {
+async function saveLocal(source, basePath = '', fileName) {
+  if (workspace.workspaceFolders === null) {
+    return;
+  }
+  const workDir = workspace.workspaceFolders[0].uri.fsPath
+  const dist = path.join(workDir, basePath, fileName);
   fs.createReadStream(source).pipe(fs.createWriteStream(dist))
+    .on("error", (e) => {
+      throw e;
+    });
+  return dist.replace(workDir, '').replace(/\\/g, '\/');
 }
 
 async function upload(config, fsPath) {
@@ -19,14 +28,17 @@ async function upload(config, fsPath) {
   const imgName = path.basename(fsPath, path.extname(fsPath))
   const fileName = `${mdFileName}_${moment().format("YYYYMMDD_HHmmss")}${path.extname(fsPath)}`
   // 当前路径的绝对路径
-  let imgUrl = `/${config.path}/${fileName}`
   const saveType = config.enable;
+  let imgUrl
+  // 保存本地
+  if (saveType === "all" || saveType === "local") {
+    imgUrl = await saveLocal(fsPath, config.basePath, fileName)
+  }
+  // 上传qcloud
   if (saveType === "all" || saveType === "qcloud") {
     imgUrl = await uploader.qUpload(utils.coverConfig(config), fsPath, fileName)
   }
-  if (saveType === "all" || saveType === "local") {
-    await saveLocal(fsPath, imgUrl)
-  }
+
   editor.edit(textEditorEdit => {
     const txt = `![${imgName}](${imgUrl})`
     textEditorEdit.insert(editor.selection.active, txt)
@@ -39,12 +51,12 @@ async function upload(config, fsPath) {
  */
 function activate(context) {
 
-  const config = workspace.getConfiguration('hugoImage')
-  if (config.enable === 'off') {
+  const config = workspace.getConfiguration('qcloudImage')
+  if (!config.enable || config.enable === 'off') {
     return;
   }
 
-  const select = commands.registerCommand('extension.hugoImage.select', async () => {
+  const select = commands.registerCommand('extension.qcloudImage.select', async () => {
     const result = await window.showOpenDialog({
       filters: { 'Images': ['png', 'jpg', 'gif', 'bmp'] }
     });
